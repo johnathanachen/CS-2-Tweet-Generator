87b3df8a1fa4028473514c2b22bb9767d4c6ecc8
Johnathan Chen <johnathanchenn@outlook.com>
2017-12-06 16:47:38 -0800

    new buttons and scraped lots of tweets

--- app/request.py
+++ app/request.py
@@ -11,11 +11,13 @@ time.sleep(1)
 
 body = browser.find_element_by_tag_name('body')
 
-for _ in range(5):
+for _ in range(700):
     body.send_keys(Keys.PAGE_DOWN)
     time.sleep(0.2)
 
 tweets = browser.find_elements_by_class_name('tweet-text')
 
+f = open("real_tweets.txt","a+")
 for tweet in tweets:
-    print(tweet.text)
+    f.write(tweet.text)
+    f.write(" ")
